25d7275da7c66e942794e5ace5033bd314eee475
matildelara <matislara739@gmail.com>
2025-06-19 01:41:18 -0600

    cambios 3

--- app.py
+++ app.py
@@ -1,7 +1,41 @@
-from flask import Flask
+from flask import Flask, request, render_template, jsonify
+import joblib
+import pandas as pd
+import logging
+
 app = Flask(__name__)
+
+# Configurar el registro
+logging.basicConfig(level=logging.DEBUG)
+
+# Cargar el modelo entrenado
+model = joblib.load("model.pkl")
+app.logger.debug("Modelo cargado correctamente.")
+
 @app.route('/')
 def home():
-    return "¡Hola, mundito!"
-if __name__ == '__main__' :
+    return render_template('formulario.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Obtener los datos enviados en el request
+        abdomen = float(request.form['abdomen'])
+        antena = float(request.form['antena'])
+
+        # Crear un DataFrame con los datos
+        data_df = pd.DataFrame([[abdomen, antena]], columns=['abdomen', 'antena'])
+        app.logger.debug(f"DataFrame enviado: {data_df}")
+
+        # Realizar predicciones
+        prediction = model.predict(data_df)
+        app.logger.debug(f"Predicción: {prediction[0]}")
+
+        # Devolver las predicciones como respuesta JSON
+        return jsonify({'categoria': prediction[0]})
+    except Exception as e:
+        app.logger.error(f"Error en la predicción: {str(e)}")
+        return jsonify({'error': str(e)}), 400
+
+if __name__ == '__main__':
     app.run(debug=True)
